d61e8d42a6ec204f457310e6cb4eac5bcb2db986
AndreVicente1 <78350660+AndreVicente1@users.noreply.github.com>
2021-02-03 12:05:23 +0100

    Update robot.py

--- robot.py
+++ robot.py
@@ -1,5 +1,6 @@
 class Robot:
-   def __init__(self,carte):
+   def __init__(self,carte,nom):
+      self.id= nom
       self.map = carte #le robot recupere la grille
       self.dir= [0,0]
       self.pos= [0,0]
