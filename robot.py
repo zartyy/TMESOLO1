f62fc43183b6eec6bfee05de8848dc1edd25e2ab
AndreVicente1 <78350660+AndreVicente1@users.noreply.github.com>
2021-02-08 14:02:25 +0100

    Update robot.py

--- robot.py
+++ robot.py
@@ -2,9 +2,15 @@ class Robot:
    def __init__(self,carte,nom):
       self.id= nom
       self.map = carte #le robot recupere la grille
-      self.dir= [0,0]
-      self.pos= [0,0]
+      self.vitesse=0.0
+      self.dir= [0.0,0.0]
+      self.pos= [0.0,0.0]
    
    def seDeplacer():
-      pos[0] = pos[0] + dir[0]
-      pos[1] = pos[1] + dir[1]
+      #à refaire avec self.vitesse.
+      
+      
+   def mapUpdate(NouvelleCarte):
+      self.map= NouvelleCarte
+    
+      
