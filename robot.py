2ed8fcd3f86891b8791ff6afe3fb7829ced500c0
Larry-space <74249212+Larry-space@users.noreply.github.com>
2021-02-08 22:02:41 +0100

    Update robot.py

--- robot.py
+++ robot.py
@@ -11,14 +11,22 @@ class Robot:
    def seDeplacer():
       self.pos[0] = self.pos[0] + self.vitesse * cos(self.angle)
       self.pos[1] = self.pos[1] + self.vitesse * sin(self.angle)
+
     
    def changerVitesse(tours_min): #prend en argument le nombre de tours par minutes en plus ou en moins voulus.
       self.vitesse = self.vitesse + (tours_min/60) # * perimetre de la roue (valeur de son rayon à demander)       
       if self.vitesse < 0.0 :
           self.vitesse = 0.0
       #demander la vitesse max du robot.
+     
+
+   def placerRobot(x, y):
+      self.dir[0] = x
+      self.dir[1] = y
+      
       
    def mapUpdate(NouvelleCarte):
       self.map= NouvelleCarte
+
     
       
