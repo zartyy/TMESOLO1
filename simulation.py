8023638a7c7d9eca2f4f01da8deeda8f7abbfe74
zartyy <simon.gr.ber@gmail.com>
2021-02-09 22:28:40 +0100

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -1,2 +1,10 @@
 from robot import *
 from arene import *
+
+# programme
+a=Arene()
+a.afficher(a.tableau)
+# binding de la fonction modifierTableau sur le canevas
+a.can.bind("<Button-1>", a.modifierTableau)
+# boucle principale
+a.init_window.mainloop()
