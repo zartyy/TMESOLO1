fd5e091aef0f5ae90c856743c424f569448a5468
Kai-Luan <78020804+Kai-Luan@users.noreply.github.com>
2021-02-10 17:12:39 +0100

    Separation de l'affichage de Arene

--- arene.py
+++ arene.py
@@ -5,22 +5,6 @@ TAILLE_ARENE_X = 25
 TAILLE_ARENE_Y = 25
 class Arene:
 	def __init__(self,matrice_x=TAILLE_ARENE_X,matrice_y=TAILLE_ARENE_Y):
-		self.init_window=Tk()
-		# fenêtre principale
-		self.init_window.title("C'est bien parti pour le 100/100")
-		self.init_window.geometry("900x575")
-		self.init_window.config(background='#41B77F')
-
-		#texte
-		self.label_title = Label(self.init_window, text="Clique gauche sur une case pour placer ou retirer un objet, le robot est dans la case rouge", font = ("",14), bg='#41B77F', fg='white')
-		self.label_title.pack(side=BOTTOM)
-		
-		#bouton
-		self.button_haut = Button(self.init_window, text="avance", command=self.avancerRobot)
-		self.button_haut.pack(side=LEFT) 
-		self.button_tourne= Button(self.init_window, text="tourne à droite", command= self.tourneRobot)
-		self.button_tourne.pack(side=LEFT)
-
 		# matrice à deux dimensions
 		self.tableau = []
 		for i in range(matrice_x):
@@ -31,51 +15,6 @@ class Arene:
 		self.robot.pos = {'x':10, 'y':10}
 		pos= self.robot.pos
 		self.tableau[int(pos['x'])][int(pos['y'])]=2 #conversion des floats en entier
-		 
-		# les 2 couleurs à utiliser
-		self.couleurs = {0: "white", 1: "#41B77F", 2: "red"}
-
-		# dimensions du canevas
-		self.can_width = 620
-		self.can_height = 620
-
-		# taille d'une case
-		self.size = 25
-
-		# création canevas
-		self.can = Canvas(self.init_window, width=self.can_width, height=self.can_height)
-		self.can.pack()
-
-
-	def afficher(self,t):
-		"""
-		Fonction d'affichage du tableau ; 1 élément = 1 case
-		La couleur de la "case" dépend de l'état de l'élement correspondant, 0 ou 1
-		"""
-		for i in range(len(t)):
-			for j in range(len(t[i])):
-				self.can.create_rectangle(i * self.size, j * self.size , i * self.size + self.size, j * self.size + self.size, fill = self.couleurs[self.tableau[i][j]])
-
-	def modifierTableau(self,evt):
-		pos_x = int(evt.x / self.size)
-		pos_y = int(evt.y / self.size)
-		# inverser la valeur de l'élément cliqué si c'est un obstacle ou une case vide
-	   	# ne fait rien si on clique sur le robot
-		if self.tableau[pos_x][pos_y] == 2:
-			self.tableau[pos_x][pos_y] = 2
-		elif self.tableau[pos_x][pos_y] == 0:
-			self.tableau[pos_x][pos_y] = 1
-		else:
-			self.tableau[pos_x][pos_y] = 0
-
-		self.afficher(self.tableau)
-
-	def simulation(self):
-		self.afficher(self.tableau)
-		# binding de la fonction modifierTableau sur le canevas
-		self.can.bind("<Button-1>", self.modifierTableau)
-		# boucle principale
-		self.init_window.mainloop()
 	
 	def avancerRobot(self):
 		x= self.robot.pos['x']
@@ -88,7 +27,6 @@ class Arene:
 			self.robot.pos= {'x': x, 'y': y}
 		pos= self.robot.pos
 		self.tableau[int(pos['x'])][int(pos['y'])]=2 #conversion des floats en entier
-		self.afficher(self.tableau)
 		
 	def tourneRobot(self):
 		self.robot.changerAngle(PI/2)
