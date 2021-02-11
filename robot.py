823caaca399f3b0affc0198c04fbe9548bf17503
Kai-Luan <78020804+Kai-Luan@users.noreply.github.com>
2021-02-11 20:27:25 +0100

    Update robot.py

--- robot.py
+++ robot.py
@@ -4,13 +4,13 @@ class Robot:
       self.id= nom
       self.map = carte #le robot recupere la grille
       self.vitesse = 0.0
-      self.pos = {'x': 0.0,'y': 0.0}
+      self.pos = [0.0,0.0]
       self.angle = 0
    
    def seDeplacer(self,time,acc):
-      self.pos['x'] = self.pos['x'] + self.vitesse * cos(self.angle) * time + 1/2 * acc * time**2
-      self.pos['y'] = self.pos['y'] + self.vitesse * sin(self.angle) * time + 1/2 * acc * time**2
-
+      self.pos[0] = self.pos[0] + self.vitesse * cos(self.angle) * time
+      self.pos[1] = self.pos[1] + self.vitesse * sin(self.angle) * time + 1/2 * acc * time**2
+      
     
    def changerVitesse(self,tours_min): #prend en argument le nombre de tours par minutes en plus ou en moins voulus.
       self.vitesse = self.vitesse + (tours_min/60) # * perimetre de la roue (valeur de son rayon à demander)       
