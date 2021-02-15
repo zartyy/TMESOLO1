0a82174ce4c46f5760a90961e54042ff733fa361
zartyy <simon.gr.ber@gmail.com>
2021-02-16 00:22:21 +0100

    ajout du module Time

--- simulation.py
+++ simulation.py
@@ -1,18 +1,8 @@
 from robot import Robot
 from arene import Arene
 from fenetre import Fenetre 
+from time import *
 
-"""
-# programme
-a=Arene()
-f= Fenetre(a)
-f.afficher()
-# binding de la fonction modifierTableau sur le canevas
-f.can.bind("<Button-1>", f.modifierTableau)
-
-# boucle principale
-f.init_window.mainloop()
-"""
 a=Arene()
 f= Fenetre(a)
 f.afficher()
@@ -23,6 +13,7 @@ f.can.bind("<Button-1>", f.modifierTableau)
 f.init_window.mainloop()
 
 while(True) :
+    time.sleep(1)
     f.afficher()
  
         
