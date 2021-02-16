fb73a26e8fab776a6e82a88636e6846ef16aec82
zartyy <simon.gr.ber@gmail.com>
2021-02-17 00:32:52 +0100

    simulation l17 wtf ???

--- simulation.py
+++ simulation.py
@@ -12,7 +12,11 @@ f.afficher()
 # binding de la fonction modifierTableau sur le canevas
 f.can.bind("<Button-1>", f.modifierTableau)
 f.button_continue.bind('<ButtonPress-1>',f.avancerRobot())
-f.button_continue.bind('<ButtonRelease-1>',f.stopRobot())
+
+#WTF plus rien marche si ca c'est actif ?? 
+#f.button_continue.bind('<ButtonRelease-1>',f.stopRobot())
+
+
 f.init_window.mainloop()
 
 f.init_window.mainloop()
