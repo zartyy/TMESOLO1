c4eadcdaa36bbcf6d10434252de1aece5ba7b962
Kai-Luan <78020804+Kai-Luan@users.noreply.github.com>
2021-03-03 08:35:50 +0100

    Ajout de la boucle dans SImulation

--- v1/simulation.py
+++ v1/simulation.py
@@ -8,8 +8,11 @@ begin = time.time()
 #une instance de robot doit etre crée
 
 a=Arene()
+c= Controler(a.robot)
 f= Fenetre(a)# ajouter le robot en paramètre
 f.afficher()
+threadf= Thread(target=f.boucle, args=(fps,))
+threadf.start()
 f.init_window.mainloop()
 
 end = time.time()
