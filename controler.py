dbe7279ed5ed9ad7de961dd559ff4a3416703634
Kai-Luan <78020804+Kai-Luan@users.noreply.github.com>
2021-03-29 09:33:53 +0200

    Update controler.py

--- controler.py
+++ controler.py
@@ -1,99 +1,72 @@
 from robot import Robot
 from math import pi as PI
 import time
+from strategy import StrategyTourneGauche, StrategyAvance
 
 class Controler:
 	def __init__(self, robot):
 		self.exit=False
 		self.robot= robot
 		self.enMarche= False
-		self.tab=[0,0,0,0,0,0,0]
-		self.action=-1
+		self.tab=[0 for i in range(7)]
+		self.actionCourant=-1
+		self.s_turnLeft= StrategyTourneGauche(self.robot)
 
 	def boucle(self,fps):
 		while True:
 			if self.exit:
 				break
-			self.update()
+			self.update(fps)
 			time.sleep(1./fps)
 
-	def update(self):
-		self.changeAction()
-		if self.action==-1:
+	def update(self, fps):
+		action=-1
+		for i in range(len(self.tab)):
+			if self.tab[i]==1:
+				action=i
+				break
+		if action==-1:
 			return
-		# Execution de 1 commande
 		print(self.tab)
-		print(self.action)
-		if self.action==0:
-			self.robot.seDeplacer(1,0)
-		elif self.action==1:
-			self.tourneRobot10()
-		elif self.action==2:
-			self.tourneRobot_10()
-		elif self.action==3:
-			self.augmenterVitesseRobot()
-		elif self.action==4:
-			self.diminuerVitesseRobot()
-		if self.action!=0:
-			self.tab[self.action]=0
-		self.action+=1
+		if action==0:
+			self.arret()
+			self.tab[action]=0
+		elif action==1:
+			self.speedUp()
+			self.tab[action]=0
+		elif action==5:
+			if not self.s_turnLeft.stop():
+				self.s_turnLeft.run(fps)
+			else: self.tab[action]=0
 
-	def changeAction(self):
-		if self.action>=len(self.tab):
-			self.action=0
-		if self.tab[self.action]!=0:
-			return
-		for i in range(self.action+1,len(self.tab)):
-			if self.tab[i]!=0:
-				self.action=i
-				return
-		if self.action==-1:
-			for i in range(0,self.action):
-				if self.tab[i]!=0:
-					self.action=i
-					return
-		self.action=-1
 
 	def signal(self, intention):
 		print("Signal recu: "+ intention)
-		if intention=="demarrer":
-			self.tab[0]=1
-		elif intention=="arreter":
-			self.tab[0]=0
+		indice=-1
+		if intention=="arret":
+			indice=0
+		elif intention=="demarrer":
+			indice=1
+		elif intention=="tournerGauche":
+			indice=5
+			self.s_turnLeft.start()
 
-		else:
-			indice=-1
-			if intention=="tourneRobot10":
-				indice=1
-			elif intention=="tourneRobot_10":
-				indice=2
-			elif intention=="augmenterVitesseRobot":
-				indice=3
-			elif intention=="diminuerVitesseRobot":
-				indice=4
 
-			if indice==-1:
-				print("Controler: Erreur indice=-1")
-			elif self.tab[indice]==0:
-				self.tab[indice]=1
-
-	def demarrer(self):
-		self.enMarche= True
+		if indice==-1:
+			print("Controler: Erreur indice=-1")
+		elif self.tab[indice]==0:
+			self.tab[indice]=1
 
 	def arret(self):
-		self.enMarche= False
+		self.robot.changerVitesseRoue(0, "LEFT")
+		self.robot.changerVitesseRoue(0, "RIGHT")
 
-	def augmenterVitesseRobot(self):
-		self.robot.changerVitesseSimple(1)
-		
-	def diminuerVitesseRobot(self):
-		self.robot.changerVitesseSimple(-1)
+	def speedUp(self):
+		self.robot.changerVitesseRoue(1, "LEFT")
+		self.robot.changerVitesseRoue(1, "RIGHT")
 
-	def tourneRobot(self):
-		self.robot.changerAngle(PI/2)
+	def turnLeft(self):
+		self.robot.changerVitesseRoue(1, "RIGHT")
 
-	def tourneRobot10(self):
-		self.robot.changerAngle(PI/18)
-	
-	def tourneRobot_10(self):
-		self.robot.changerAngle(-PI/18)
+	def turnRight(self):
+		self.robot.changerVitesseRoue(1, "LEFT")
