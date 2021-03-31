fe355f567aa09cdd1529c3961ea6db14e6812672
Kai-Luan <78020804+Kai-Luan@users.noreply.github.com>
2021-03-31 08:40:50 +0200

    Update controler.py

--- controler.py
+++ controler.py
@@ -1,9 +1,10 @@
+# coding: utf-8
 from robot import Robot
 from math import pi as PI
 import time
 from strategy import StrategyTourneGauche, StrategyAvance
 
-class Controler:
+class Controler(object):
 	def __init__(self, robot):
 		self.exit=False
 		self.robot= robot
@@ -11,6 +12,8 @@ class Controler:
 		self.tab=[0 for i in range(7)]
 		self.actionCourant=-1
 		self.s_turnLeft= StrategyTourneGauche(self.robot)
+		self.s_forward= StrategyAvance(self.robot)
+		self.s_carre= StrategyTracerCarre()
 
 	def boucle(self,fps):
 		while True:
@@ -28,44 +31,45 @@ class Controler:
 		if action==-1:
 			return
 		print(self.tab)
+		# s'arrêter
 		if action==0:
-			self.arret()
+			self.robot.stop()
 			self.tab[action]=0
+		# Avancer
 		elif action==1:
-			self.speedUp()
-			self.tab[action]=0
+			if not self.s_forward.stop():
+				self.s_forward.run(fps)
+			else: self.tab[action]=0
+		# tracer un carre
+		elif action==4:
+			if not self.s_carre.stop():
+				self.s_carre.run(fps)
+			else: self.tab[action]=0
+		# Tourner
 		elif action==5:
 			if not self.s_turnLeft.stop():
 				self.s_turnLeft.run(fps)
 			else: self.tab[action]=0
-
 
 	def signal(self, intention):
 		print("Signal recu: "+ intention)
 		indice=-1
 		if intention=="arret":
 			indice=0
-		elif intention=="demarrer":
+		elif intention=="avancer":
 			indice=1
+			self.s_forward.start(5)
+		elif intention=="tournerGauche":
+			indice=4
+			self.s_carre.start()
 		elif intention=="tournerGauche":
 			indice=5
 			self.s_turnLeft.start(0)
-
+		elif intention=="tournerDroite":
+			indice=5
+			self.s_turnLeft.start(1)
 
 		if indice==-1:
 			print("Controler: Erreur indice=-1")
 		elif self.tab[indice]==0:
 			self.tab[indice]=1
-	def arret(self):
-		self.robot.set_motor_dps(0, "LEFT")
-		self.robot.set_motor_dps(0, "RIGHT")
-
-	def speedUp(self):
-		self.robot.set_motor_dps(1, "LEFT")
-		self.robot.set_motor_dps(1, "RIGHT")
-
-	def turnLeft(self):
-		self.robot.set_motor_dps(1, "RIGHT")
-
-	def turnRight(self):
-		self.robot.set_motor_dps(1, "LEFT")
