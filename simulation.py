1b788a53c8891fa90a752c36db76ea928116420e
Kai-Luan <78020804+Kai-Luan@users.noreply.github.com>
2021-03-31 11:04:53 +0200

    Ajout du Robot Reel

--- simulation.py
+++ simulation.py
@@ -7,6 +7,7 @@ import time
 
 r=0
 fps=20
+"""
 try:
 	from robot2I013 import Robot2I013
 	r= Robot2I013()
@@ -14,6 +15,8 @@ except:
 	from gopigo import Robot2I013
 	from robot import Robot_Proxy
 	r= Robot_Proxy([], Robot2I013())
+	"""
+r= Robot([])
 
 c= Controler(r)
 a=Arene(r)
